14a78713751538707bf2c144937d989a8cfec652
KIM SOO HYUN STREAM <321288247+kingsoohyun@users.noreply.github.com>
2026-09-21 18:16:54 +0900

    .

--- events.js
+++ events.js
@@ -676,3 +676,20 @@ loadEvents().catch(err => {
             "<p>지도를 불러오지 못했습니다.</p>";
     }
 });
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
